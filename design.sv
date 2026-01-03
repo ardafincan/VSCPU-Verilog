9f304663e0951711e30392fdcfa460da0ff9d900
Ali Arda Fincan <ardafincan@icloud.com>
2026-01-03 16:20:00 +0300

    added ADD, NAND instructions. added a macro for Program Counter incrementing

--- design.sv
+++ design.sv
@@ -8,6 +8,9 @@ reg [2:0] st, stN;
 reg [13:0] PC, PCN;
 reg [31:0] IW, IWN;
 reg [31:0] R1, R1N;
+
+`define INCPC PCN = PC + 14'd1
+
 always @(posedge clk) begin
     st <= stN;
     PC <= PCN;
@@ -30,38 +33,86 @@ always @ * begin
         case (st)
             3'd0: begin // S0: Fetch State
                 addr_toRAM = PC;
-                stN = 3'd1;
+				stN = 3'd1;
             end
             3'd1: begin // S1: Decode State
-                IWN = data_fromRAM;
+                IWN = data_fromRAM;	
+				if(data_fromRAM[31:28] == 4'b0000)// ADD
+					begin 
+					addr_toRAM = data_fromRAM[27:14];
+					stN = 3'd2;
+				end 
                 if(data_fromRAM[31:28] == 4'b0001)//ADDi
-                begin
-                addr_toRAM = data_fromRAM[27:14];
-                stN = 3'd2;
-                end
+					begin
+					addr_toRAM = data_fromRAM[27:14];
+					stN = 3'd2;
+				end
+				if(data_fromRAM[31:28] == 4'b0010) // NAND
+					begin 
+					addr_toRAM = data_fromRAM[27:14];
+					stN = 3'd2;
+				end
+				if(data_fromRAM[31:28] == 4'b0011) // NANDi
+					begin 
+					addr_toRAM = data_fromRAM[27:14];
+					stN = 3'd2;
+				end
                 if(data_fromRAM[31:28] == 4'b1000) // CP
-                begin
-                addr_toRAM = data_fromRAM[13:0];
-                stN = 3'd2;
-                end
-                end
+					begin
+					addr_toRAM = data_fromRAM[13:0];
+					stN = 3'd2;
+				end
+            end
             3'd2: begin // S2: Decode/Execute State
+				if (IW[31:28] == 4'b0000) begin // ADD
+					R1N = data_fromRAM;
+					addr_toRAM = IW[13:0];
+					stN = 3'd3;
+				end
                 if (IW[31:28]==4'b0001) begin // ADDi
                     wrEn = 1'b1;
                     addr_toRAM = IW[27:14];
                     data_toRAM = data_fromRAM + IW[13:0];
-                    PCN = PC + 14'd1;
-                    stN = 3'd0;
-                    end
-                    if (IW[31:28]==4'b1000) begin // CP
-                    wrEn = 1'b1;
-                    addr_toRAM = IW[27:14];
-                    data_toRAM = data_fromRAM;
-                    PCN = PC + 14'd1;
-                    stN = 3'd0;
-                    end
-
-            	end
+					`INCPC;
+					stN = 3'd0;
+                end
+				if (IW[31:28] == 4'b0010) begin // NAND
+					R1N = data_fromRAM;
+					addr_toRAM = IW[13:0];
+					`INCPC;
+					stN = 3'd3;
+				end
+				if (IW[31:28] == 4'b0011) begin // NANDi
+					wrEn = 1'b1;
+					addr_toRAM = IW[27:14];
+					data_toRAM = ~(data_fromRAM & IW[13:0]);
+					`INCPC;
+					stN = 3'd0;
+				end 
+				if (IW[31:28]==4'b1000) begin // CP
+					wrEn = 1'b1;
+					addr_toRAM = IW[27:14];
+					data_toRAM = data_fromRAM;
+					`INCPC;
+					stN = 3'd0;
+				end
+            end
+			3'd3: begin // S3 Execute Extended
+				if (IW[31:28] == 4'b0000) begin // ADD
+					wrEn = 1'b1;
+					addr_toRAM = IW[27:14];
+					data_toRAM = R1 + data_fromRAM; 
+					`INCPC;
+					stN = 3'd0;
+				end
+				if (IW[31:28] == 4'b0010) begin // NAND
+					wrEn = 1'b1;
+					addr_toRAM = IW[27:14];
+					data_toRAM = ~(R1 & data_fromRAM);
+					`INCPC;
+					stN = 3'd0;
+				end
+			end
 
 endcase
 end // else
